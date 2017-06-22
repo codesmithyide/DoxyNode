ba9ada8e56aa1efd477d6fad94d8f3e079fa3eda
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-06-23 00:01:56 +0100

    Updates

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var doxynode = require("../dist/codesmithy-doxynode.js")
+
 var tf = require("ishiko-test-framework")
 
 let theTestHarness = new tf.TestHarness("codesmithy-doxynode")
