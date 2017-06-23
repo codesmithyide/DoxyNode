1070a6e3fde8efc2648afd0a9b907a178e74b61d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-06-23 10:08:52 +0100

    Added files

---
+++ test/DoxygenXMLOutputTests.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let testSequence = theTestHarness.appendTestSequence("DoxygenXMLOutput tests")
+}
