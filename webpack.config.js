0f6670ba88c2e690eb8aa35e69dd3446693537f3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-06-24 17:15:37 +0100

    Removed external dependencies from distribution

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,25 @@ module.exports = {
         libraryTarget: "umd",
         filename: "codesmithy-doxynode.js",
         path: path.resolve(__dirname, "dist")
+    },
+    externals: {
+        path: {
+            commonjs: "path",
+            commonjs2: "path",
+            amd: "path",
+            root: "path"
+        },
+        fs: {
+            commonjs: "fs",
+            commonjs2: "fs",
+            amd: "fs",
+            root: "fs"
+        },
+        xml2js: {
+            commonjs: "xml2js",
+            commonjs2: "xml2js",
+            amd: "xml2js",
+            root: "xml2js"
+        }
     }
 }
