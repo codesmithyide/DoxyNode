90a22a1e26cc56945614c1165a35ec5914f7a569
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-06-26 21:32:49 +0100

    Updates

--- test/FunctionDocumentationTests.js
+++ test/FunctionDocumentationTests.js
@@ -5,4 +5,12 @@ var doxynode = require("../dist/codesmithy-doxynode.js")
 
 module.exports = function(theTestHarness) {
     let testSequence = theTestHarness.appendTestSequence("FunctionDocumentation tests")
+
+    new tf.FunctionBasedTest("Creation test 1", FunctionDocumentationCreationTest1, testSequence)
+}
+
+function FunctionDocumentationCreationTest1(resolve, reject)
+{
+    let functiondocumentation = new doxynode.FunctionDocumentation()
+    resolve(tf.TestResultOutcome.ePassed)
 }
