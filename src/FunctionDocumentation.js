47f2461a4d3ea7ba014336f3ddd6fba33872597b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-06-26 10:19:46 +0100

    Added files

---
+++ src/FunctionDocumentation.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export class FunctionDocumentation {
+
+    constructor() {
+    }
+
+}
---
+++ test/FunctionDocumentationTests.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var tf = require("ishiko-test-framework")
+var doxynode = require("../dist/codesmithy-doxynode.js")
+
+module.exports = function(theTestHarness) {
+    let testSequence = theTestHarness.appendTestSequence("FunctionDocumentation tests")
+}
