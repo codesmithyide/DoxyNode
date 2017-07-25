fdcd9924250599e78912f838ee8707d3a772ec10
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-07-25 10:14:57 +0100

    Updates

--- test/data/cpp-code-3/Rectangle.h
+++ test/data/cpp-code-3/Rectangle.h
@@ -7,16 +7,16 @@ public:
     void draw() override;
 
     /// Gets the width of the rectangle.
-    /// @returns The width of the rectangle.
+    /// @return The width of the rectangle.
     int width() const;
     /// Gets the height of the rectangle.
-    /// @returns The height of the rectangle.
+    /// @return The height of the rectangle.
     int height() const;
 
     /// Sets the width of the rectangle.
     /// @param w The new width.
     void setWidth(int w);
     /// Sets the height of the rectangle.
-    /// @param w The new height.
+    /// @param h The new height.
     void setHeight(int h);
 };
