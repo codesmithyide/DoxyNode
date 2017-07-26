29a04dace1f78c68829e4d29cb5f21a6471f3566
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-07-26 10:24:41 +0100

    Updated example

--- examples/gettingstarted/index.js
+++ examples/gettingstarted/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+'use strict'
+
+// The example refers to the local distribution but when using
+// the npm package you would simply require "codesmithy-doxynode.js"
+var doxynode = require("../../dist/codesmithy-doxynode.js")
+
+let xmloutput = new doxynode.DoxygenXMLOutput()
+xmloutput.initialize("data/xml").then(function() {
+    console.log("hello")
+})
